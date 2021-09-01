3c4ce5284e6178469f211ee3e5d08a333437be77
AliChaudharySandhu <alichaudhary100@gmail.com>
2021-09-01 21:50:10 +0500

    Product Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,9 @@ import React from 'react'
 export const Header = () => {
     return (
         <header className="bg-info text-white p-2">
-            <h2>React Shopping Cart</h2>
+            <div className="container-fluid">
+                <h2>React Shopping Cart</h2>
+            </div>
         </header>
     )
 }
---
+++ src/components/Products.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import './products.css'
+
+export class Products extends Component {
+    constructor(props){
+        super();
+
+    }
+    render() {
+        return (
+            <div>
+                <ul className="products d-inline-flex flex-wrap list-unstyled justify-content-between">
+                    {this.props.products.map((product) =>{
+                        return(
+                            <li className="mb-5" key={product._id}>
+                                <div className="product">
+                                    <a className="text-capitalize text-dark" href={"#" + product._id}>
+                                        <img src={product.image} alt={product.name}/>
+                                        <p className="mt-2 text-truncate">{product.title}</p>
+                                    </a>
+                                    <div className="product_price d-flex flex-row align-items-center justify-content-between">
+                                        <p className="mb-0 mx-3">${product.price}</p>
+                                        <button className="btn btn-md btn-warning text-white">Add To Cart</button>
+                                    </div>
+                                </div>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Products
